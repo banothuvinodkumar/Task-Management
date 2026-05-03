8861c89c1c3c712b40e41e8aebcc39d0b6638c88
banothuvinodkumar <vinodkumarbanothu855@gmail.com>
2026-05-03 15:06:19 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 // @desc    Register new user
@@ -20,11 +21,15 @@ const registerUser = async (req, res) => {
     throw new Error('User already exists');
   }
 
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   // Create user
   const user = await User.create({
     name,
     email,
-    password,
+    password: hashedPassword,
   });
 
   if (user) {
