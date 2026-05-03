2940fce30e826ffa938b699829c1a0a320ae857d
banothuvinodkumar <vinodkumarbanothu855@gmail.com>
2026-05-03 15:38:45 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(error);
     process.exit(1);
   }
 };
